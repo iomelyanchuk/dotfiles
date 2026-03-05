6cc6e43c70d012ccb97aefe9c9c129dcd3467c3b
Illia Omelianchuk <iom.mailbox@gmail.com>
2026-03-05 13:53:24 +0200

    Updates (treesitter break changes)

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,15 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = { "vim", "regex", "lua", "bash", "markdown", "markdown_inline" },
-      sync_install = false,
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+  -- config = function()
+  --   local configs = require("nvim-treesitter.setup")
+  --   configs.setup({
+  --     ensure_installed = { "vim", "regex", "lua", "bash", "markdown", "markdown_inline" },
+  --     sync_install = false,
+  --     auto_install = true,
+  --     highlight = { enable = true },
+  --     indent = { enable = true },
+  --   })
+  -- end
 }
